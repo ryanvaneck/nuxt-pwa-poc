f415a2bd8040ae2e549109bf6e7a557a0335a18c
Ryan Van Eck <ryan.vaneck@endurance.com>
2020-08-08 15:03:41 -0600

    add layout

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="footer">footer</div>
+</template>
+
+
+<style>
+  .footer {
+    position: fixed;
+    bottom: 0;
+  }
+</style>
---
+++ components/PageLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header/>
+      <slot/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+  import Header from './Header';
+  import Footer from './Footer';
+  export default {
+    components: {
+      Header,
+      Footer,
+    }
+  }
+</script>
